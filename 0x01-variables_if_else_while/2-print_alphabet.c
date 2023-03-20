82cf3c40f4bfdb059ad9da7016b0ed950abfe54e
Yassine-AitSimmou0 <yassinegames6@gmail.com>
2023-03-20 15:01:01 -0700

    print_alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints tha alphabet in lowercase
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	putchar (x);
+
+	putchar ('\n');
+	return (0);
+}
