f5e58d683552e9ab98172b63fec211d4ccb1fb4f
Yassine-AitSimmou0 <yassinegames6@gmail.com>
2023-03-31 19:52:42 -0700

    7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @str: string
+ * Return: str
+ **/
+
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	char letters[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
+
+	while (str[i])
+	{
+		while (j < 10)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet[j];
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (str);
+}
